a2156cb9e73573f0a3df4e9c1576c779cce6f91b
WillMGRichter <wric0006@student.monash.edu>
2025-04-30 15:02:01 +1000

    Added inline comments for Action function in monster

--- server/src/model/game/monster.ts
+++ server/src/model/game/monster.ts
@@ -33,15 +33,19 @@ class monster {
      * enemy: monster, Pointer to the enemy monster class
      * enemy_roll: number, Number rolled by enemy
      */
-    var AC: number = this.ac;
+    var AC: number = this.ac; //Establish AC as current allowing for flexibility with defence action
     if (!action) {
+      // if defence is selcted increment ac and decrement defence turns
       AC += 2;
       this.defence -= 1;
     }
     if (enemy_action) {
+      // When enemy is attacking
       if (enemy_roll + enemy.attack_bonus > AC) {
+        //Checks to see if enemy hits
         var damage: number = 5;
         if (enemy_roll == 20) {
+          //Critical damage
           damage = 10;
         }
         this.hp -= damage;
