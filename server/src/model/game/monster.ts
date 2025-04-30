edefe08fdb0c99bb03378f4ce41bb9bcbb458b56
WillMGRichter <wric0006@student.monash.edu>
2025-04-30 14:34:18 +1000

    Created constructor for monster

--- server/src/model/game/monster.ts
+++ server/src/model/game/monster.ts
@@ -1,14 +1,29 @@
-class monster{
-    type: string; 
-    hp: number;
-    attack: number;
-    defend: number;
-     constructor(type: string,hp: number,attack: number, defend: number){
-        this.type = type
-        this.hp = hp
-        this.attack = attack
-        this.defend = defend 
-     }
+class monster {
+  type: string;
+  hp: number;
+  attack: number;
+  ac: number;
+  defence: number;
+  constructor(
+    type: string,
+    hp: number,
+    attack: number,
+    defend: number,
+    bonus: number,
+    defence: number
+  ) {
+    this.type = type;
+    this.hp = hp;
+    this.attack = attack;
+    this.ac = defend;
+    this.defence = defence;
+    this.bonus = bonus;
+  }
+  attackAction(enemy: monster): void {}
+}
 
-
+class monsterX extends monster {
+  constructor() {
+    super("MonterX", 10, 10, 10);
+  }
 }
