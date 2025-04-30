f7bb6ac20be57b552783b80bb7bfe342d704cb45
OmarSalem248 <osal0004@student.monash.edu>
2025-04-30 14:22:40 +1000

    made constructor for monster

---
+++ server/src/model/game/monster.ts
@@ -0,0 +1,14 @@
+class monster{
+    type: string; 
+    hp: number;
+    attack: number;
+    defend: number;
+     constructor(type: string,hp: number,attack: number, defend: number){
+        this.type = type
+        this.hp = hp
+        this.attack = attack
+        this.defend = defend 
+     }
+
+
+}
