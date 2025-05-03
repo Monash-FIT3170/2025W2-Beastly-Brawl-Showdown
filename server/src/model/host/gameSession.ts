4a2917acf21171f83462d4e5c2a0f33b4659ea05
Daniel Loh <danielhyloh@gmail.com>
2025-05-03 16:29:42 +1000

    added a feature to gameSession where it will now generate a six digit gameCode on construction.
    Also has functions to getGameCode and checkGameCode

--- server/src/model/host/gameSession.ts
+++ server/src/model/host/gameSession.ts
@@ -1,16 +1,20 @@
 
 import Player from "../game/player";
 import Queue from "../../utils/queue";
-
 export default class GameSession{
     //need to confirm what attributes
     hostUID: string;
     players: Queue<Player>; //need to setup queue
+    private gameCode : Number;
 
 
     constructor(hostID: string){
+        //constructor that generates six digit gameCode upon startup
         this.hostUID = hostID;
         this.players = new Queue<Player>(8);
+
+        const generatedSixDigitCode = (): Number => parseInt(Math.floor(Math.random() * 1000000).toString().padStart(6, '0'), 10);
+        this.gameCode = generatedSixDigitCode();
     }
 
     public addPlayer(player: Player){
@@ -24,6 +28,17 @@ export default class GameSession{
             return false;
         }
         return true;
+    }
+    
+    public getGameCode(){
+        return this.gameCode;
+    }
+
+    public checkGameCode(inputCode: Number){
+        if(inputCode == this.gameCode){
+            return true;
+        }
+        return false;
     }
 
 }
