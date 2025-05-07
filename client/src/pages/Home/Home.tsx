a7bbd74a54a02ad715cf2b4d96f3ea291e791fd4
cameron <chum0001@student.monash.edu>
2025-05-08 00:59:12 +1000

    2003: Installed Tailwind -Tailwind install allows for access to all tailwind features

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { ButtonDemo } from '../../components/buttons/Button';
 
 export const Home = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
+    <ButtonDemo/>
   </div>
 );
