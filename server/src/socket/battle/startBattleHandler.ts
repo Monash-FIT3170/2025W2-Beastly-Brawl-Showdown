2bd83ff4a12e0c09107ffc480a7eda8e29ab2cfb
devanfedhi <devanfedhi1@gmail.com>
2025-05-12 19:00:42 +1000

    Backend mostly finalised

--- server/src/socket/battle/startBattleHandler.ts
+++ server/src/socket/battle/startBattleHandler.ts
@@ -19,93 +19,94 @@ export const startBattleHandler = (io: Server, socket: Socket) => {
       });
 
       io.to(battleId).emit("battle_started", battleId);
-      proceedBattleTurn(io, battle)
+      proceedBattleTurn(io, battle);
+    }
+  });
+};
 
-  }});
-}
+// TODO: separate this function into a separate file
+function proceedBattleTurn(io: Server, battle: Battle) {
+  // TODO: Set a property in the battle instance to object it is in the 10 sec waiting stage (for the host match summary page)
+  battle.clearBattleLogs();
+  battle.incTurn();
 
+  let playersInBattle = battle.getPlayers();
 
-function proceedBattleTurn(io: Server, battle:Battle){
-        // TODO: separate below logic into a separate function, so that it can be reused every turn
-        battle.clearBattleLogs();
-        battle.incTurn();
-  
-        let playersInBattle = battle.getPlayers();
-  
-        playersInBattle.forEach((player) => {
-          io.to(player.getId()).emit(
-            "battle_state",
-            battle.getBattleState(player.getId())
-          ); // Emit the battle state to each player
-  
-          let actions = player.getMonster().getPossibleActionStates();
-          io.to(player.getId()).emit("possible_actions", actions); // Emit the list of action names
-        });
-  
-        let player1 = playersInBattle[0];
-        let player2 = playersInBattle[1];
-  
-        let timer = 10; // Set the initial timer value (e.g., 60 seconds)
-        const interval = setInterval(() => {
-          if (timer >= 0) {
-            io.to(battle.getId()).emit("timer", timer);
-            timer--;
-          } else {
-            // Turn is over, conduct the "battle" logic
-            clearInterval(interval);
-  
-            // Check each player in battle has a selected action
-            playersInBattle.forEach((player) => {
-              if (player.getActions().length === 0) {
-                player.addAction(new NullAction());
-              }
-            });
+  playersInBattle.forEach((player) => {
+    io.to(player.getId()).emit(
+      "battle_state",
+      battle.getBattleState(player.getId())
+    ); // Emit the battle state to each player
 
-            // Prepare method
-            player1.getActions().forEach((action) => {
-              action.prepare(player1, player2);
-            });
-  
-            player2.getActions().forEach((action) => {
-              action.prepare(player2, player1);
-            });
-  
-            // Execute method
-            player1.getActions().forEach((action) => {
-              action.execute(player1, player2);
-            });
-  
-            player2.getActions().forEach((action) => {
-              action.execute(player2, player1);
-            });
-  
-            console.log("P1: ", player1);
-  
-            console.log("P2: ", player2);
-  
-            // Emite the result of the battle state after the turn is complete
-            playersInBattle.forEach((player) => {
-              io.to(player.getId()).emit(
-                "battle_state",
-                battle.getBattleState(player.getId())
-              );
-            });
-  
-            // After results of actions are sent to the client, and client has updated its UI, need to reset the stats of player back to Monster
-            playersInBattle.forEach((player) => {
-              player.resetStats();
-              player.resetActions();
-            });
-            
-            if (battle.isBattleOver()){
-              io.to(battle.getId()).emit("battle_end",battle.getWinner())
-            }
-          
-          setTimeout(() => {
-            proceedBattleTurn(io, battle)
-          }, 2000)
+    let actions = player.getMonster().getPossibleActionStates();
+    io.to(player.getId()).emit("possible_actions", actions); // Emit the list of action names
+  });
+
+  let player1 = playersInBattle[0];
+  let player2 = playersInBattle[1];
+
+  let timer = 10; // Set the initial timer value (e.g., 60 seconds)
+  const interval = setInterval(() => {
+    if (timer >= 0) {
+      io.to(battle.getId()).emit("timer", timer);
+      timer--;
+    } else {
+      // Turn is over, conduct the "battle" logic
+      clearInterval(interval);
+
+      // Check each player in battle has a selected action
+      playersInBattle.forEach((player) => {
+        if (player.getActions().length === 0) {
+          player.addAction(new NullAction());
         }
-    }, 1000); // Emit every second
+      });
+
+      // Prepare method
+      player1.getActions().forEach((action) => {
+        action.prepare(player1, player2);
+      });
+
+      player2.getActions().forEach((action) => {
+        action.prepare(player2, player1);
+      });
+
+      // Execute method
+      player1.getActions().forEach((action) => {
+        action.execute(player1, player2);
+      });
+
+      player2.getActions().forEach((action) => {
+        action.execute(player2, player1);
+      });
+
+      console.log("P1: ", player1);
+
+      console.log("P2: ", player2);
+
+      // Emite the result of the battle state after the turn is complete
+      playersInBattle.forEach((player) => {
+        io.to(player.getId()).emit(
+          "battle_state",
+          battle.getBattleState(player.getId())
+        );
+      });
+
+      // After results of actions are sent to the client, and client has updated its UI, need to reset the stats of player back to Monster
+      playersInBattle.forEach((player) => {
+        player.resetStats();
+        player.resetActions();
+      });
+
+      if (battle.isBattleOver()) {
+        io.to(battle.getId()).emit("battle_end", battle.getWinner());
+      }
+
+      // TODO: Set a 2-5 second "animation" window, and set a property in the battle instance to object it is in the animation stage (for the host match summary page)
+
+      setTimeout(() => {
+        // TODO: Set a property in the battle instance to object it is in the battle result stage (for the host match summary page)
+        proceedBattleTurn(io, battle);
+      }, 2000);
+    }
+  }, 1000); // Emit every second
 }
-
-
