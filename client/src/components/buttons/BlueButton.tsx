26a6f7a1246515c1970bab696ce2eed72ee2e61d
cameron <chum0001@student.monash.edu>
2025-05-13 14:27:11 +1000

    2003: Updated main.css woth custom colours and fonts - @theme specifies customer colours using hex codes -@theme imports jua and lets it be called normally in tailwind -BlueButton class acts as a default blue button that can be used in the game

---
+++ client/src/components/buttons/BlueButton.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+interface ButtonProps{
+    text: string;
+    onClick: () => void;
+}
+
+export const BlueButton = ({text, onClick}: ButtonProps) => {
+    return(
+    <button 
+    className=  "bg-customblue text-[50px] text-white font-[Jua] py-5 px-10 rounded-[15px] border border-[4px] border-darkpurple"
+    onClick={onClick}
+    >
+  {text}
+</button>
+)
+}
--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import { ButtonDemo } from '../../components/buttons/Button';
 import { CardDemo } from '../../components/cards/Card';
 import { InfoTextDemo } from '../../components/texts/InfoText';
+import { BlueButton } from '../../components/buttons/BlueButton';
 
 export const Home = () => (
   <div>
     <h1>Welcome to Meteor!</h1>
+    <BlueButton text = 'Defend' onClick={() => (console.log("hey"))}/>
     <ButtonDemo/>
     <CardDemo />
   </div>
