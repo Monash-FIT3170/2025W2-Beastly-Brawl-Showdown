a3492224554450efb59d63f875b6cadee5668e8f
Luna Nagle <pnag0009@student.monash.edu>
2025-05-14 14:53:44 +1000

    2003: Add component for user text input.

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,15 +1,16 @@
 import React from 'react';
 import { ButtonDemo } from '../../components/buttons/Button';
 import { CardDemo } from '../../components/cards/Card';
-import { InfoTextDemo } from '../../components/texts/InfoText';
+import { InputBox } from '../../components/inputs/InputBox'
 import { BlueButton } from '../../components/buttons/BlueButton';
 import { Header } from '../../components/cards/Header';
 
-export const Home = () => (
-  <div>
+export const Home = () => ( 
+  <div> 
     <Header text="Welcome to Meteor!" />
     <BlueButton text = 'Defend' onClick={() => (console.log("hey"))}/>
     <ButtonDemo/>
+    <InputBox />
     <CardDemo />
   </div>
 );
