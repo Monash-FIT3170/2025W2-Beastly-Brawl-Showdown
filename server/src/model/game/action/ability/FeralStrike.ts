d01494e96f2a64518e8081bd595e067e82ceba97
OmarSalem248 <osal0004@student.monash.edu>
2025-05-14 14:03:49 +1000

    made new abilities

---
+++ server/src/model/game/action/ability/FeralStrike.ts
@@ -0,0 +1,24 @@
+import { Action } from "../action";
+import { Player } from "../../player";
+import { ActionIdentifier } from "/types/single/actionState";
+
+export class FeralStrikeAbilityAction extends Action {
+  constructor() {
+    super(
+      ActionIdentifier.FERAL_STRIKE,
+      "Feral Strike",
+      "Deals extra damage on critical hits."
+    );
+  }
+
+  public prepare(actingPlayer: Player, affectedPlayer: Player): void {}
+
+  public execute(actingPlayer: Player, affectedPlayer: Player): void {
+    actingPlayer.addLog(
+      `You did nothing. Unimplemented action ${this.getName()}`
+    );
+    affectedPlayer.addLog(
+      `${actingPlayer.getName()} did nothing. Unimplemented action ${this.getName()}`
+    );
+  }
+}
---
+++ server/src/model/game/action/ability/ShadowLeap.ts
@@ -0,0 +1,24 @@
+import { Action } from "../action";
+import { Player } from "../../player";
+import { ActionIdentifier } from "/types/single/actionState";
+
+export class ShadowLeapAbilityAction extends Action {
+  constructor() {
+    super(
+      ActionIdentifier.SHADOW_LEAP,
+      "Shadow Leap",
+      "Can evade an attack once per battle."
+    );
+  }
+
+  public prepare(actingPlayer: Player, affectedPlayer: Player): void {}
+
+  public execute(actingPlayer: Player, affectedPlayer: Player): void {
+    actingPlayer.addLog(
+      `You did nothing. Unimplemented action ${this.getName()}`
+    );
+    affectedPlayer.addLog(
+      `${actingPlayer.getName()} did nothing. Unimplemented action ${this.getName()}`
+    );
+  }
+}
