5f0efbbc0f95a5f374e4d1930bbf577ae7be188c
cameron <chum0001@student.monash.edu>
2025-05-14 14:26:51 +1000

    2003: Created a card for monster selection - Card contains monster name, description, image and type - Switch case prevents having to have multiple cards for different monster types

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -5,11 +5,14 @@ import { InfoTextDemo } from '../../components/texts/InfoText';
 import { BlueButton } from '../../components/buttons/BlueButton';
 import { RedButton } from '../../components/buttons/RedButton';
 import { PurpleHeaderCard } from '../../components/cards/PurpleHeaderCard';
+import { MonsterSelectionCard } from '../../components/cards/MonsterSelectionCard';
 
 export const Home = () => (
   <>
     <PurpleHeaderCard text = "SELECT YOUR MONSTER"/>
     <h1>Welcome to Meteor!</h1>
+
+    <MonsterSelectionCard name = 'Dragon' description='flavourtext' image = 'path' type = 'defender'/>
     <BlueButton text = 'DEFEND' onClick={() => (console.log("hey"))}/>
     <RedButton text = 'ATTACK' onClick = {() => console.log("attack")}></RedButton>
     <ButtonDemo/>
