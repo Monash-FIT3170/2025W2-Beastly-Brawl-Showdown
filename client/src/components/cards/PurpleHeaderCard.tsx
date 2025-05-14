c2a540e3df31a4ba942245d02af13c1a8d67eb41
Danniel You <dyou0009@student.monash.edu>
2025-05-14 14:02:49 +1000

    2003: Improve design of the PurpleHeaderCard - Modified Cameron's PurpleHeaderCard to look more like my Header, using tailwind - Removed my old Header code, which is not needed anymore

--- client/src/components/cards/PurpleHeaderCard.tsx
+++ client/src/components/cards/PurpleHeaderCard.tsx
@@ -6,7 +6,7 @@ interface HeaderProps{
 
 export const PurpleHeaderCard = ({text}: HeaderProps) => {
     return(
-    <div className=  "bg-brightpurple text-large text-white font-[Jua] w-fit h-normalPhoneHeight rounded-[15px] border border-[4px] border-darkpurple border-t-0">
+    <div className="mx-auto bg-brightpurple text-large text-white font-[Jua] w-fit h-normalPhoneHeight rounded-bl-xl rounded-br-xl border-[4px] border-darkpurple border-t-0 text-center text-wrap pl-[1%] pr-[1%]">
         {text}
     </div>
     );
