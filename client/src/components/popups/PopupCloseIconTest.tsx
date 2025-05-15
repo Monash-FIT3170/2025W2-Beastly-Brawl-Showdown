cb72407c53ce602e2f58c10df8a194d2366a64e7
Tinesia <tyuu0023@student.monash.edu>
2025-05-15 19:47:01 +1000

    feature/2003-Design-System-Components: added the css component so that when mouse hover over buttons and the cross icon in popup, it turns into a pointer.

--- client/src/components/popups/PopupCloseIconTest.tsx
+++ client/src/components/popups/PopupCloseIconTest.tsx
@@ -6,7 +6,7 @@ interface IconXProps {
 
 export const IconXPopup = ({ onClick }: IconXProps) => {
   return(
-    <svg className="text-darkpurple size-8" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" onClick={onClick}>
+    <svg className="text-darkpurple size-8 cursor-pointer hover:opacity-80" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" onClick={onClick}>
       <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
     </svg>
   )
