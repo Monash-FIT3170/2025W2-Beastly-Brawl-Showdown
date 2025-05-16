121cfe175235a1a80bcc9bb5d82988b41f9f06a7
Danniel You <dyou0009@student.monash.edu>
2025-05-16 14:09:54 +1000

    2003: Renamed property in TotalHealthBar for consistency

--- client/src/components/bars/TotalHealthBar.tsx
+++ client/src/components/bars/TotalHealthBar.tsx
@@ -3,11 +3,11 @@ import { Bar } from "./Bar";
 
 interface TotalHealthBarProps {
     totalHealth: number;
-    highestHealth: number;
+    highestTotalHealth: number;
 }
 
-export const TotalHealthBar = ({totalHealth, highestHealth}: TotalHealthBarProps) => {
+export const TotalHealthBar = ({totalHealth, highestTotalHealth}: TotalHealthBarProps) => {
     return (
-        <Bar colour="neongreen" text={String(totalHealth)} textPosition="right" radius="lg" fillPercentage={Math.floor(totalHealth / highestHealth * 100)} />
+        <Bar colour="neongreen" text={String(totalHealth)} textPosition="right" radius="lg" fillPercentage={Math.floor(totalHealth / highestTotalHealth * 100)} />
     );
 }
