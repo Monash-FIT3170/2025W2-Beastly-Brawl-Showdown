575ede8afa41c168b4de1f00f0f2a6266fb8dccc
Naveen Sellathurai <Navboy44@outlook.com>
2025-05-19 21:25:12 +1000

    Feature/3001.2 remove player on request (#24)
    * Added method to remove a given player from the GameSession's queue
    
    - Uses a for loop, dequeue and enqueue to run through the loop via serving and pushing each player object in the players Queue without pushing the player that is the given argument
    - Currently nothing will happen if the player given in the argument is not in the queue at all
    
    * Testing to see if commits are done under the right account
    
    - Due to using another email for another project in another unit, the previous commit for the remoePlayer method was unintentionally done under the wrong account
    - git email has been changed, so this commit is purely to test if it worked
    
    * Removing test line
    
    - email change worked
    - NOTE: commit done under 'The99thCourier' was done by Naveen. That was the account username for the other email
    
    * Modifying the removePlayer method to check IDs instead
    
    - Argument changed from Player to a userID string
    - If statement now checks the ID of the player that was dequeued
    
    ---------
    
    Co-authored-by: Naveen Sellathurai <nsel0009@student.monash.edu>
    Co-authored-by: derek-j-cao <derek.jcao@gmail.com>

--- server/src/model/host/gameSession.ts
+++ server/src/model/host/gameSession.ts
@@ -1,18 +1,16 @@
-
 import Player from "../game/player";
 import Queue from "../../utils/queue";
 
 export default class GameSession{
     //need to confirm what attributes
     hostUID: string;
-    players: Queue<Player>; //need to setup queue
-
+    players: Queue<Player>;
     private gameCode : Number;
+  
     constructor(hostID: string, presetGameCode?: number) {
         this.hostUID = hostID;
         this.players = new Queue<Player>(8);
     
-        
         // Constructor that generates six digit gameCode upon startup or uses preset code
         if (presetGameCode !== undefined) {
             // Use preset game code if provided
@@ -32,6 +30,19 @@ export default class GameSession{
           ) {
         this.players.enqueue(player)
           }
+    }
+
+    /*
+    Function takes a player object as an argument, and then the queue is run through by serving each item until it has looped through the entire queue. If a served item is not the same as the player given in the argument, then it is pushed back into the queue, but if it is the same, then it is not pushed back into the queue  
+    */
+    public removePlayer(removingPlayerID: String) {
+        // Loop to check through each item in the queue
+        for (let i = 0; i < this.players.size(); i++) {
+            const playerIndexed = this.players.dequeue(); // Serves the current player at the front
+            if (playerIndexed != undefined && playerIndexed.userID != removingPlayerID) {
+                this.players.enqueue(playerIndexed); // If the player is not the argument one, then put them back in the queue
+            }
+        }
     }
 
     public canStartGame(): boolean {
