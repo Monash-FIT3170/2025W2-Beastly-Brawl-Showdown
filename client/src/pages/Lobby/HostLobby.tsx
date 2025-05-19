c21efa8be58c011c776345364aa847b781739eb1
derek-j-cao <derek.jcao@gmail.com>
2025-05-19 21:33:22 +1000

    Feature/3003 lobby page (#25)
    * Move lobby code to published branch
    
    * Add frontend player type
    
    * Add display for web address
    
    * Make functions more compatabile with backend
    
    * Remove rendering of HostLobby
    
    * Consistent spacing
    
    ---------
    
    Co-authored-by: Luna Nagle <pnag0009@student.monash.edu>

---
+++ client/src/pages/Lobby/HostLobby.tsx
@@ -0,0 +1,51 @@
+import React, { useState } from 'react';
+import Player from '../../types/player' 
+
+export const HostLobby = () => {
+    const [playerCount, setPlayerCount] = useState(3);
+    const [code, setCode] = useState(468923);
+    const [players, setPlayers] = useState<Player[]>([
+      new Player("0", "Player 1"), 
+      new Player("1", "Player 2"), 
+      new Player("2", "Player 3")
+    ]);
+
+    const addPlayer = (playerName: string, playerID: string) => {
+      setPlayers(players.concat(new Player(playerID, playerName)))
+      setPlayerCount(playerCount + 1);
+    }
+
+    const removePlayer = (playerID: string) => {
+      setPlayers(players.filter((Player) => Player.userID !== playerID));
+      setPlayerCount(playerCount - 1);
+    };
+
+    const enterCode = (newCode: number) => {
+      setCode(newCode)
+    }
+
+    const kickPlayer = (playerID: string) => {
+      removePlayer(playerID)
+      // backend player removal call
+    };
+
+    const startGame = () => {
+ 
+    }
+  
+    return (
+      <div>
+        <p>Beastly Brawl</p>
+        <p>Hosting at address at: {window.location.origin}</p>
+        <p>Join the game with: {code}</p>
+        <p>Currently there are {playerCount}/8 players in the lobby.</p>
+        {players.map((player) => (
+          <div key={player.userID}>
+            <p>{player.name}</p> 
+            <button onClick={() => kickPlayer(player.userID)}>Remove</button>
+          </div>
+        ))}
+        <button onClick={startGame} disabled={playerCount < 2}>Start Game</button>
+      </div>
+    );
+  };
---
+++ client/src/types/player.ts
@@ -0,0 +1,10 @@
+export default class Player {
+    public userID: string;
+    public name: string;
+
+    constructor(userID: string, name: string) {
+        this.userID = userID;
+        this.name = name;
+      
+    }
+}
