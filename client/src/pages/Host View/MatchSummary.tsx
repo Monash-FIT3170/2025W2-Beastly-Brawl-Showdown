a77a55c992e9546a92bbff5c3f8ac5f394ffbd89
Mubashar Doostizadah <mubashardoostizadah@gmail.com>
2025-05-20 14:38:51 +1000

    Add background to MatchSummary screen

--- client/src/pages/Host View/MatchSummary.tsx
+++ client/src/pages/Host View/MatchSummary.tsx
@@ -1,14 +1,9 @@
 import React from 'react';
-import Background from '../../components/background/Background';
+import GameBackground from '../../components/background/Background';
 
 const MatchSummary = () => {
   return (
-    <Background imagePath="/path/to/test-image.jpg">
-      <div style={{ color: 'white', padding: '20px' }}>
-        <h1>Testing Background Component</h1>
-        <p>If you see the background image behind this text, it works!</p>
-      </div>
-    </Background>
+    <GameBackground />
   );
 };
 
