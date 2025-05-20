2797092a0ad5ecf5cdc852d2f0fd317a2f3c46cd
Derek Cao <derek.jcao@gmail.com>
2025-05-20 16:13:02 +1000

    feat: added frontend structure for join lobby

--- client/main.tsx
+++ client/main.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { Meteor } from "meteor/meteor";
-// import { Home } from "./src/pages/Home/Home";
+import { Home } from "./src/pages/Home/Home";
 import { HostLobby } from "./src/pages/Lobby/HostLobby";
+import { JoinLobby } from "./src/pages/Lobby/JoinLobby";
 
 Meteor.startup(() => {
   const container = document.getElementById("react-target");
   const root = createRoot(container!);
   root.render(<HostLobby />);
+  // root.render(<Home />);
+  // root.render(<JoinLobby />);
 });
