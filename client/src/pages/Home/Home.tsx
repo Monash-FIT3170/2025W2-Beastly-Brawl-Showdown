314c5a9872a4291a91ccc5951365622da0682989
Luna Nagle <pnag0009@student.monash.edu>
2025-05-23 12:53:44 +1000

    2004: Style home page

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { LogoDisplay } from "../../components/logo/Logo";
 import { FlowRouter } from "meteor/ostrio:flow-router-extra";
 import socket from "../../socket";
+import { ButtonGeneric } from "../../components/buttons/ButtonGeneric";
+import { OutlineText } from "../../components/texts/OutlineText";
+import { LogoResizable } from "../../components/logos/LogoResizable";
 
 export const Home = () => {
   // Called on 'Host Lobby' button press
@@ -24,10 +27,22 @@ export const Home = () => {
   return (
     console.log("Home"),
     (
-      <div>
-        <LogoDisplay size="3xl" />
-        <button onClick={createGame}>Host Game</button>
-        <button onClick={renderJoinLobby}>Join Game</button>
+      <div className="bg-peach p-[1.25rem] h-screen w-screen flex flex-col justify-around">
+        <div className="bg-goldenRod h-full outline-blackCurrant outline-[0.25rem] rounded-2xl flex flex-col justify-around items-center">
+          <LogoResizable height={20} width={20}></LogoResizable>
+          <div className="flex flex-col items-center justify-center w-1/2 h-1/2 space-y-10">
+            <ButtonGeneric color="ronchi" size="large" onClick={createGame}>
+              <OutlineText size="large">
+                Host Game
+              </OutlineText>
+            </ButtonGeneric>
+            <ButtonGeneric color="ronchi" size="large" onClick={renderJoinLobby}>
+              <OutlineText size="large">
+                Join Game
+              </OutlineText>
+            </ButtonGeneric>
+          </div>
+        </div>
       </div>
     )
   );
