c5f3ee8be25be306ff509bbaba96c5dde3d45ca3
yuchaeflowers <atra0066@student.monash.edu>
2025-05-27 18:37:16 +1000

    1001.1: Separated battle and host health bars and monsters - Temporary fix

--- client/src/components/player-screen/HealthBar.tsx
+++ client/src/components/player-screen/HealthBar.tsx
@@ -4,20 +4,17 @@ import "./HealthBar.css";
 interface HealthBarProps {
   current: number;
   max: number;
-  isLeftPlayer?: boolean|null;
 }
 
-const HealthBar: React.FC<HealthBarProps> = ({ current, max, isLeftPlayer = true }) => {
+const HealthBar: React.FC<HealthBarProps> = ({ current, max }) => {
   const percentage = (current / max) * 100;
 
   let color = "green";
-
-  if (percentage === 0) color = "grey";
-  else if (percentage <= 20) color = "red";
+  if (percentage <= 20) color = "red";
   else if (percentage <= 50) color = "orange";
 
   return (
-    <div className="health-bar" style={{ position: 'relative' }}>
+    <div className="health-bar">
       <div
         className="health-bar-fill"
         style={{
@@ -25,21 +22,8 @@ const HealthBar: React.FC<HealthBarProps> = ({ current, max, isLeftPlayer = true
           backgroundColor: color,
         }}
       >
+        <span className="health-bar-text">{current}/{max}</span>
       </div>
-      <span 
-        className="health-bar-text"
-        style={{
-          position: 'absolute',
-          top: '50%',
-          transform: 'translateY(-50%)',
-          left: isLeftPlayer ? '8px' : 'auto',
-          right: isLeftPlayer ? 'auto' : '8px',
-          zIndex: 10,
-          pointerEvents: 'none',
-        }}
-      >
-        {current}/{max}
-      </span>
     </div>
   );
 };
