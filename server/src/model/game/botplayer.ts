7550b3f5266db98028cc538b15743020d9c387a0
OmarSalem248 <osal0004@student.monash.edu>
2025-08-01 13:01:54 +1000

    bots can get a random name

--- server/src/model/game/botplayer.ts
+++ server/src/model/game/botplayer.ts
@@ -4,9 +4,22 @@ import { PlayerState } from "/types/single/playerState";
 import {Player} from "server/src/model/game/player"
 import { RockyRhino } from "./monster/rockyRhino";
 
-export class botplayer extends Player{
-  constructor() {
-    super("placeHolder", "Big Bum Loser")
+export class BotPlayer extends Player {
+  private static nameList = [
+    "Big Bum Loser",
+    "Chimp on a typewriter",
+    "George",
+    "Brain in a jar",
+    "Very intelligent fetus"
+  ];
+
+  private static getRandomName(): string {
+    const index = Math.floor(Math.random() * BotPlayer.nameList.length);
+    return BotPlayer.nameList[index];
   }
-  
+
+  constructor() {
+    const randomName = BotPlayer.getRandomName();
+    super("placeholder",randomName);
+  }
 }
