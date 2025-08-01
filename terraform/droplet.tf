f090366a725685793b6095159b0c5dbb25719084
devanfedhi <devanfedhi1@gmail.com>
2025-08-01 23:08:03 +1000

    Feature/2003: Setup initial terraform code - Created the initial terraform code and pipeline.

---
+++ terraform/droplet.tf
@@ -0,0 +1,7 @@
+resource "digitalocean_droplet" "droplet_bbs_game_server" {
+  image   = var.droplet_image
+  name    = var.droplet_name
+  region  = var.region
+  size    = var.droplet_size
+  ssh_keys = [data.digitalocean_ssh_key.selected.id]
+}
