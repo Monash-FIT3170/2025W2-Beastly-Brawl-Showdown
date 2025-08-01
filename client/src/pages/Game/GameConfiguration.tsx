2125cb6ab15a5ad1d832f2812c0d8bcd6d2774e3
Luna Nagle <pnag0009@student.monash.edu>
2025-08-01 13:37:44 +1000

    1001.7: Add configuration page for choosing a type of tournament.

---
+++ client/src/pages/Game/GameConfiguration.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { FlowRouter } from "meteor/ostrio:flow-router-extra";
+import socket from "../../socket";
+import { ButtonGeneric } from "../../components/buttons/ButtonGeneric";
+import { OutlineText } from "../../components/texts/OutlineText";
+import LogoResizable from "../../components/logos/LogoResizable";
+import { BlankPage } from "../../components/pagelayouts/BlankPage";
+
+
+export const GameConfiguration = () => {
+  // Called on 'Host Lobby' button press
+  const createGame = () => {
+    socket.emit("create-game", {});
+    console.log("Game session created");
+  };
+
+  // Received when Game Session is created. Takes user to 'Host Lobby' Page
+  socket.on("new-game", ({ code }) => {
+    const codeString = code.toString();
+    FlowRouter.go(`/host/${codeString}`);
+  });
+
+  // Called on 'Join Lobby' button press
+  const createRoyale = () => {
+    // FlowRouter.go("/join");
+  };
+
+  return (
+    console.log("Home"),
+    ( 
+      <BlankPage>
+        <div className="flex flex-row h-1/2 w-full sm:items-end lg:items-center justify-around">
+          <LogoResizable className="lg:w-1/4 sm:h-3/4 lg:h-full"></LogoResizable>
+        </div>
+        <div className="flex flex-col items-center justify-center w-1/2 h-1/2 lg:space-y-10 sm:space-y-30">
+            <div className="flex flex-row items-center justify-center h-1/2 space-x-10">
+                <ButtonGeneric color="ronchi" size="large" onClick={createGame}>
+                    <OutlineText size="large">SINGLE ROUND</OutlineText>
+                </ButtonGeneric>
+                <ButtonGeneric color="ronchi" size="large" onClick={createRoyale}>
+                    <OutlineText size="large">BATTLE ROYALE</OutlineText>
+                </ButtonGeneric>
+            </div>
+            <div className="flex flex-row items-center justify-center h-1/2 space-x-10">
+            <ButtonGeneric color="ronchi" size="large">
+                <OutlineText size="large">ROUND ROBIN</OutlineText>
+            </ButtonGeneric>
+            </div>
+        </div>
+      </BlankPage>
+    )
+  );
+};
