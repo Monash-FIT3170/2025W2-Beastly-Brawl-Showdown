62a3becf136ca066f175863882c5baea5b833afe
devanfedhi <devanfedhi1@gmail.com>
2025-08-01 23:08:03 +1000

    2001: Setup initial terraform code - Created the initial terraform code and pipeline.

---
+++ terraform/terraform.tfvars
@@ -0,0 +1,6 @@
+ssh_key_name = "ssh-bbs"
+region = "syd1"
+
+droplet_name = "droplet-bbs-game-server"
+droplet_size = "s-2vcpu-4gb"
+droplet_image = "ubuntu-22-04-x64"
---
+++ terraform/variables.tf
@@ -0,0 +1,19 @@
+variable "ssh_key_name" {
+  type        = string
+}
+
+variable "region" {
+  type        = string
+}
+
+variable "droplet_name" {
+  type        = string
+}
+
+variable "droplet_image" {
+  type        = string
+}
+
+variable "droplet_size" {
+  type        = string
+}
