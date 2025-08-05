5c300f5f9d8ec02b98215430201822cb43b8693b
WillMGRichter <159985026+WillMGRichter@users.noreply.github.com>
2025-08-05 18:25:17 +1000

    2002: Using await to recieved promised data to fix  user found error

--- server/src/socket/backend/registerHandler.ts
+++ server/src/socket/backend/registerHandler.ts
@@ -2,14 +2,14 @@ import { Server, Socket } from "socket.io";
 import { getPlayerData,insertNewPlayer } from "../../database/dbManager";
 
 export const registerHandler = (io: Server, socket: Socket) => {
-    socket.on('register',(data) => {
+    socket.on('register', async (data) => {
         const  {email,username,password} = data;
         if(!email || !username|| !password){
             socket.emit('registerResponse',{success:false,message: 'Missing Fields'});
             return;
         }
 
-        const user = getPlayerData(email);
+        const user = await getPlayerData(email);
         if(user){
             socket.emit('registerResponse',{success:false,message:'User already exists'});
             return;
