53043e904941d7b164bc72ca26548e7ce6bc71ad
Huu Nguyen <hduyon4@gmail.com>
2025-08-05 18:46:55 +1000

    Feature/1006.1-bot-socket: Prevent socket emitting for bot player; allow the player to compete against bot player with limited move

--- server/src/socket/battle/startBattleHandler.ts
+++ server/src/socket/battle/startBattleHandler.ts
@@ -52,8 +52,7 @@ export default function proceedBattleTurn(
   });
 
   playersInBattle.forEach((player) => {
-    if (!player.isBotPlayer()) {
-      //only emit to socket if the player is a human
+    if (!player.isBotPlayer()){ //only emit to socket if the player is a human
       io.to(player.getId()).emit(
         "battle_state",
         battle.getBattleState(player.getId())
@@ -115,8 +114,7 @@ export default function proceedBattleTurn(
       // TODO: For the future, actions should trigger their own animations themselves. Perhaps add a feature that emits animation type and let the
       // battle screen handle the type of animation to show
       player1.getActions().forEach((action) => {
-        if (!player1.isBotPlayer()) {
-          //only emit to socket if the player is a human
+        if (!player1.isBotPlayer()){ //only emit to socket if the player is a human
           if (action.getName() === "Attack") {
             const attackAction = action as AttackAction;
             const diceRoll = attackAction.getDiceRoll();
@@ -135,8 +133,7 @@ export default function proceedBattleTurn(
       });
 
       player2.getActions().forEach((action) => {
-        if (!player2.isBotPlayer()) {
-          //only emit to socket if the player is a human
+        if (!player2.isBotPlayer()){ //only emit to socket if the player is a human
           if (action.getName() === "Attack") {
             const attackAction = action as AttackAction;
             const diceRoll = attackAction.getDiceRoll();
