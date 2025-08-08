4a6b5d48c14f2aafb639c9a8d2eaa30be3c9ee4d
WillMGRichter <159985026+WillMGRichter@users.noreply.github.com>
2025-08-08 11:04:36 +1000

    Feature/2002 login merge into database branch (#49)
    * 2002: Implemented trial login system
     -Setup Sockets
     -Attempted homepage login button with popup
    
    * 2002: Implementing register feature to login popup
    
    * 2002: Fixed import error
    
    * 2002: Using await to recieved promised data to fix  user found error
    
    * 2002: Added await to  login command  to allow for validation of user details

--- server/src/socket/backend/registerHandler.ts
+++ server/src/socket/backend/registerHandler.ts
@@ -1,25 +1,39 @@
 import { Server, Socket } from "socket.io";
-import { getPlayerData,insertNewPlayerAccount } from "../../database/dbManager";
+import {
+  getPlayerData,
+  insertNewPlayerAccount,
+} from "../../database/dbManager";
 
 export const registerHandler = (io: Server, socket: Socket) => {
-    socket.on('register', async (data) => {
-        const  {email,username,password} = data;
-        if(!email || !username|| !password){
-            socket.emit('registerResponse',{success:false,message: 'Missing Fields'});
-            return;
-        }
+  socket.on("register", async (data) => {
+    const { email, username, password } = data;
+    if (!email || !username || !password) {
+      socket.emit("registerResponse", {
+        success: false,
+        message: "Missing Fields",
+      });
+      return;
+    }
 
-        const user = await getPlayerData(email);
-        if(user){
-            socket.emit('registerResponse',{success:false,message:'User already exists'});
-            return;
-        }
-        try{
-            insertNewPlayerAccount(email,username,password);
-            socket.emit('registerResponse',{success:true,message:'Registration Successful'});
-        } catch (error){
-            socket.emit('registerResponse',{success:false,message:error.message});
-        }
-        
-    });
-}
+    const user = await getPlayerData(email);
+    if (user) {
+      socket.emit("registerResponse", {
+        success: false,
+        message: "User already exists",
+      });
+      return;
+    }
+    try {
+      insertNewPlayerAccount(email, username, password);
+      socket.emit("registerResponse", {
+        success: true,
+        message: "Registration Successful",
+      });
+    } catch (error) {
+      socket.emit("registerResponse", {
+        success: false,
+        message: error.message,
+      });
+    }
+  });
+};
