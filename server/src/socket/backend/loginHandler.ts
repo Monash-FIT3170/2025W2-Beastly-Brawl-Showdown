a45499617e276fd5a9c54101fc532f372e1fde49
Meng Siau <mengsiau@gmail.com>
2025-08-10 00:20:39 +1000

    feature/2001-Player logging in will now bind their account to Player

--- server/src/socket/backend/loginHandler.ts
+++ server/src/socket/backend/loginHandler.ts
@@ -1,5 +1,6 @@
 import { Server, Socket } from "socket.io";
 import { getPlayerData } from "../../database/dbManager";
+import { playerAccounts } from "../../../main";
 
 export const loginHandler = (io: Server, socket: Socket) => {
     socket.on('login', async (data) => {
@@ -8,12 +9,19 @@ export const loginHandler = (io: Server, socket: Socket) => {
             socket.emit('loginResponse',{success:false,message: 'Missing Fields'});
             return;
         }
-
+        
+        // Verify user credentials
         const user = await getPlayerData(email);
         if(!user||user.password !== password){
             socket.emit('loginResponse',{success:false,message:'Invalid Email or Password'});
             return;
         }
+
+        // Successful login
         socket.emit('loginResponse',{success:true,message:'Login Successful'});
+
+        // Update the player account in the playerAccounts{socketID: PlayerAccount} map with the logged-in user data
+        playerAccounts.set(socket.id, user);
+        console.log(`Login Successful: Player account updated for socket ID: ${socket.id} with email: ${user.email}`);
     });
 }
