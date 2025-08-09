40d2f6f54eb5ddc9b0a4ebc343315ca4c602624e
Meng Siau <mengsiau@gmail.com>
2025-08-10 00:20:39 +1000

    feature/2001-Player logging in will now bind their account to Player

--- server/src/database/dbManager.ts
+++ server/src/database/dbManager.ts
@@ -66,6 +66,7 @@ function createPlayerMonsterStatSchema(monsterId: string,): PlayerMonsterStatSch
 }
 
 // Used for initializing a Player Account that represents a Guest. Will be overwritten when users register/logins
+// NOTE: For now, default/guest accounts have blank fields. May need to refactor this. 
 export function createDefaultPlayerAccountSchema(): PlayerAccountSchema {
   return {
     email: '',
