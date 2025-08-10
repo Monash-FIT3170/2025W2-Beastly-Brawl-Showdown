fd8165ac8f4d4d93e0dcc4905b717ee95db7fd7b
Anika Kamleshwaran <anikakamleshwaran@gmail.com>
2025-08-11 00:10:20 +1000

    3002: updated status to keep expired statuses for the remainder of the turn

--- server/src/model/game/status/status.ts
+++ server/src/model/game/status/status.ts
@@ -4,32 +4,33 @@ export abstract class Status {
   public name: string;
   protected description: string;
   protected countDown: number;
- 
 
-  constructor(name: string, description: string, countDown: number=0) {
+  constructor(name: string, description: string, countDown: number = 0) {
     this.name = name;
     this.description = description;
     this.countDown = countDown;
   }
 
   public tick(player: Player): void {
-    this.effect(player);
+    if (this.countDown > 0) {
+      //necessary check as UI requires status to remain post countdown = 0
+      this.effect(player);
+    }
     this.countDown -= 1;
-  } 
+  }
 
   public abstract effect(player: Player): void;
 
-  public getName(): string{
+  public getName(): string {
     return this.name;
   }
 
   public isExpired(): boolean {
     //checks if countdown hit 0
-    if(this.countDown <= 0){
+    if (this.countDown < 0) {
       return true;
     }
 
     return false;
   }
-    
 }
