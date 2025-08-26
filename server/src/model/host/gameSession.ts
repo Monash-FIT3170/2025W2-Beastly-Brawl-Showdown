e95e410ddef3ee569b1ba0f25c3a0daabb1425a3
OmarySalem248 <omarys248@gmail.com>
2025-08-27 00:24:21 +1000

    Feature/1006.1 bot socket (#57)
    * made bot player and set up randomised monster selection
    
    * bots can get a random name
    
    * Feature/1006.1-bot-socket: Minor adjustment to the player model;
    - Add an attribute to indicate whether a player is a bot or human
    
    * Feature/1006.1-bot-socket: Make sure the bot does not automatically die when matched with a player
    
    * Feature/1006.1-bot-socket: Prevent socket emitting for bot player; allow the player to compete against bot player with limited move
    
    * 1006.1: added action randomiser and had the socket call upon it
    so far the 'randomiser' only adds attack actions
    
    * 1.1 acciidentally made a new branch and commit
    
    * 1006.1:  made a new branch accedentally and had to merge some changes from there onto here
    continued debugging action randomiser, attempted to make it so only viable actions are used, further debugging may be required
    
    * 1006.1 : updated bot logic
    now bots can randomlly choose avaliable actions to hit their oponnents but cant use actions that theyve fully used
    
    * made bot player and set up randomised monster selection
    
    * bots can get a random name
    
    * Feature/1006.1-bot-socket: Minor adjustment to the player model;
    - Add an attribute to indicate whether a player is a bot or human
    
    * Feature/1006.1-bot-socket: Make sure the bot does not automatically die when matched with a player
    
    * Feature/1006.1-bot-socket: Prevent socket emitting for bot player; allow the player to compete against bot player with limited move
    
    * 1006.1: added action randomiser and had the socket call upon it
    so far the 'randomiser' only adds attack actions
    
    * 1.1 acciidentally made a new branch and commit
    
    * 1006.1:  made a new branch accedentally and had to merge some changes from there onto here
    continued debugging action randomiser, attempted to make it so only viable actions are used, further debugging may be required
    
    * 1006.1 : updated bot logic
    now bots can randomlly choose avaliable actions to hit their oponnents but cant use actions that theyve fully used
    
    * 1006.1: fixed up code and naming conventions to respond to feeback on PR, made it so that the name of a bot can be chosen if needed
    
    * 1006.1 updated game session to fix merge issue
    
    * 1006.1: continued to clean up code from mess made by merging
    
    * Rename botplayer.ts to botPlayer.ts
    
    - can't do via personal ide
    
    * Rename actionrandomiser.ts to actionRandomiser.ts
    
    - can't do via personal ide
    
    * 1006.1: debugged status issue with random actions
    - updated getpossibleactions to give temporaryactions
    - added console errors/logs in action randomiser
    - updated randomaction to be static method
    
    * 1006.1: updated monster randomiser
    - moved into botPlayer as requested
    - utilised monsterMap instead of variable monsterList
    
    ---------
    
    Co-authored-by: OmarSalem248 <osal0004@student.monash.edu>
    Co-authored-by: Huu Nguyen <hduyon4@gmail.com>
    Co-authored-by: Derek Cao <derek.jcao@gmail.com>
    Co-authored-by: Anika Kamleshwaran <117716223+kamlesa@users.noreply.github.com>
    Co-authored-by: Anika Kamleshwaran <anikakamleshwaran@gmail.com>

--- server/src/model/host/gameSession.ts
+++ server/src/model/host/gameSession.ts
@@ -9,6 +9,9 @@ import { BattlePhase } from "../../../../types/composite/battleState";
 import { PlayerState } from "/types/single/playerState";
 import { MonsterIdentifier } from "/types/single/monsterState";
 import { RockyRhino } from "../game/monster/rockyRhino";
+import { PouncingBandit } from "../game/monster/pouncingBandit";
+import { CinderTail } from "../game/monster/cinderTail";
+import { BotPlayer } from "../game/botPlayer";
 import crypto from "crypto";
 import { PouncingBandit } from "../game/monster/pouncingBandit";
 import { CinderTail } from "../game/monster/cinderTail";
@@ -26,7 +29,7 @@ export default class GameSession {
   private player_max: number = 120; // Max 120 players
   private battle_max: number = 60; // Max 60 battles
   private currentPhase: BattlePhase = BattlePhase.CHOOSE_ACTION;
-  private monsters: Array<String>;
+  // private monsters: Array<String>;
   private mode: IGameMode;
   
   // Initialise sample data
@@ -39,7 +42,7 @@ export default class GameSession {
     // POST-MVP: increase max players and battles
     this.players = new Queue<Player>(this.player_max);
     this.battles = new Queue<Battle>(this.battle_max);
-    this.monsters = ["RockyRhino","PouncingBandit","CinderTail"];
+    // this.monsters = ["RockyRhino","PouncingBandit","CinderTail"];
 
     if (addition.presetGameCode !== undefined) {
       // Use preset game code if provided
@@ -95,9 +98,9 @@ export default class GameSession {
   public getPlayers() {
     return this.players;
   }
-  public getMonsters(){
-    return this.monsters;
-  }
+  // public getMonsters() {
+  //   return this.monsters;
+  // }
 
   // Add player to Game Session queue
   public addPlayer(player: Player): { success: boolean; reason?: string } {
@@ -245,31 +248,21 @@ export default class GameSession {
     return this.battles;
   }
 
-  public oddOneOutWinner(oddPlayer: Player) {
+  public oddOneOutWinner(oddPlayer: Player): Player {
     let battleId = crypto.randomUUID();
-    const placeHolderPlayer = new BotPlayer()
-    const placerHolderMonster = this.monsters[Math.floor(Math.random() * 3)];
-    if (placerHolderMonster == "RockyRhino"){
-      placeHolderPlayer.setMonster(new RockyRhino());
-    }
-    if (placerHolderMonster == "PouncingBandit"){
-      placeHolderPlayer.setMonster(new PouncingBandit());
-    } 
-    if (placerHolderMonster == "CinderTail"){
-      placeHolderPlayer.setMonster(new CinderTail());
-    }           
-    placeHolderPlayer.setHealth(0);
-    const battle = new Battle(
-      battleId,
-      oddPlayer,
-      placeHolderPlayer,
-      this.hostUID
-    );
+
+    const botPlayer = new BotPlayer();
+    botPlayer.setRandomMonster(); //moved original code inside bot player
+
+    this.players.enqueue(botPlayer);
+
+    const battle = new Battle(battleId, oddPlayer, botPlayer, this.hostUID);
+
     battles.set(battleId, battle);
     this.battles.enqueue(battle);
+
     return oddPlayer;
   }
-
   public calculateMostChosenMonster() {
     // Map from monster name to { monster: Monster, count: number }
     const monsterCount: Record<string, { monster: Monster; count: number }> =
