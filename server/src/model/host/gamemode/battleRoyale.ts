bf8ec42c772e6044a3433739103661f205a61a71
Danniel You <dyou0009@student.monash.edu>
2025-08-30 17:25:13 +1000

    1008.1: Did more cleanup after the merge, including removing some duplicate code

--- server/src/model/host/gamemode/battleRoyale.ts
+++ server/src/model/host/gamemode/battleRoyale.ts
@@ -12,7 +12,6 @@ export class BattleRoyale implements IGameMode {
   private eliminatedPlayers: Player[] = [];  // Earlier eliminated players are closer to the front of the array
   private remainingPlayers: Player[] = [];
   private io: Server | null = null;
-  private socket: Socket | null = null;
   private playerFinished = 0;
 
   public init(session: GameSession, io: Server, socket: Socket): void {
@@ -42,17 +41,104 @@ export class BattleRoyale implements IGameMode {
 
     console.log("[ELIMINATED PLAYERS]: ", this.eliminatedPlayers.map(player => player.getName()));
     console.log("[REMAINING PLAYERS]: ", this.remainingPlayers.map(player => player.getName()));
-  
+
     if (winner != null) {
       io.sockets.sockets.get(winner.getId())?.emit("battle-closed", {gameCode : session.getGameCode().toString()})
     }
-    const p1 = io.sockets.sockets.get(battle.getPlayers()[0].getId())
-    console.log("After battle p1: ", p1)
+    const p1 = io.sockets.sockets.get(battle.getPlayers()[0].getId());
+    console.log("After battle p1: ", p1);
 
+    io.sockets.sockets.get(battle.getPlayers()[0].getId())?.once("ready_next_battle", () => {
+      console.log("Server test 1 (Royale)", io.sockets.sockets.get(battle.getPlayers()[0].getId())?.id);
+      if (winner != null) {
+        session.getWaitQueue().enqueue(winner);
+        console.log(`Add player ${winner} to waitQueue`);
+        if (session.getWaitQueue().size() > 1) {
+          console.log("waitQueue Size large enough");
+
+          const player1Indexed = session.getWaitQueue().dequeue();
+          const player2Indexed = session.getWaitQueue().dequeue();
+
+          if (player1Indexed != undefined && player2Indexed != undefined) {
+          
+            let battleId = crypto.randomUUID();
+
+            const battle = new Battle(
+              battleId,
+              player1Indexed,
+              player2Indexed,
+              session.getHost()
+            );
+            battles.set(battleId, battle);
+            session.getBattles().enqueuefront(battle);
+
+            console.log(`Connecting to ${socket.id}`);
+            for (const player of battle.getPlayers()) {
+              io.sockets.sockets.get(player.getId())?.join(battle.getId());
+            }
+            console.log(`Check to start battle ${battle.getId()}`);
+            io.to(battle.getId()).emit("battle_started", battle.getId());
+            proceedBattleTurn(io, socket, session, battle);
+          }
+        }
+      }
+      this.playerFinished += 1;
+      console.log(this.playerFinished,session.getPlayers().getItems().length);
+      if (this.playerFinished == session.getPlayers().getItems().length) {
+        this.onBattlesEnded(session, io, socket);
+      }
+    });
+
+    io.sockets.sockets.get(battle.getPlayers()[1].getId())?.once("ready_next_battle", () => {
+      console.log("Server test 2 (Royale)", io.sockets.sockets.get(battle.getPlayers()[0].getId())?.id);
+      if (winner != null) {
+        session.getWaitQueue().enqueue(winner);
+        console.log(`Add player ${winner} to waitQueue`);
+        if (session.getWaitQueue().size() > 1) {
+          console.log("waitQueue Size large enough");
+
+          const player1Indexed = session.getWaitQueue().dequeue();
+          const player2Indexed = session.getWaitQueue().dequeue();
+
+          if (player1Indexed != undefined && player2Indexed != undefined) {
+          
+            let battleId = crypto.randomUUID();
+
+            const battle = new Battle(
+              battleId,
+              player1Indexed,
+              player2Indexed,
+              session.getHost()
+            );
+            battles.set(battleId, battle);
+            session.getBattles().enqueuefront(battle);
+
+            console.log(`Connecting to ${socket.id}`);
+            for (const player of battle.getPlayers()) {
+              io.sockets.sockets.get(player.getId())?.join(battle.getId());
+            }
+            console.log(`Check to start battle ${battle.getId()}`);
+            io.to(battle.getId()).emit("battle_started", battle.getId());
+            proceedBattleTurn(io, socket, session, battle);
+          }
+        }
+      }
+      this.playerFinished += 1;
+      console.log(this.playerFinished,session.getPlayers().getItems().length);
+      if (this.playerFinished == session.getPlayers().getItems().length) {
+        this.onBattlesEnded(session, io, socket);
+      }
+    });
+  }
+
+  /*
+   * TODO: Need to handle the rare case where every single ongoing battle ends in a draw
+   */
+	public onBattlesEnded(session: GameSession, io: Server, socket: Socket): void {
     if (this.isSessionConcluded(session)) {
-      // Handle if it is a draw or not
+      // Handle if the final results are a draw or not
       let top2;
-      if (this.remainingPlayers.length != 0) {  // Is not a draw (i.e., 1 remaining payer - the winner)
+      if (this.remainingPlayers.length != 0) {  // Is not a draw (i.e., 1 remaining player - the winner)
         let winner = this.remainingPlayers[0];
         let runnerUp = this.eliminatedPlayers[this.eliminatedPlayers.length-1];
         top2 = [  // Top 2 are the winner and runner up
@@ -60,121 +146,19 @@ export class BattleRoyale implements IGameMode {
           runnerUp.getPlayerState()
         ];
         console.log(`[FINAL RESULTS]: Winner: ${winner.getName()}, Runner up: ${runnerUp.getName()}`);
-      } else {  // Is a draw
-        console.log(this.eliminatedPlayers.map(p => p.getName()))
-        let player1 = battle.getPlayers()[0].getPlayerState();
-        let player2 = battle.getPlayers()[1].getPlayerState();
-        top2 = [player1, player2];
-        console.log(`[FINAL RESULTS]: Draw between: ${top2[0].name} and ${top2[1].name}`);
+      } else {  // Is a draw (i.e., 0 remaining players)
+        let drawP1 = this.eliminatedPlayers[this.eliminatedPlayers.length-2];
+        let drawP2 = this.eliminatedPlayers[this.eliminatedPlayers.length-1];
+        top2 = [
+          drawP1.getPlayerState(),
+          drawP2.getPlayerState()
+        ];
+        console.log(`[FINAL RESULTS]: Draw between: ${drawP1.getName()} and ${drawP2.getName()}`);
       }
       const gameCode = session.getGameCode();
       this.io?.to(`game-${gameCode}`).emit("final-results-response", { playersToDisplay: top2 });
       session.setFinalResults(top2);
     }
-
-    io.sockets.sockets.get(battle.getPlayers()[0].getId())?.once("ready_next_battle", () => {
-      console.log("Server test 1 (Royale)", io.sockets.sockets.get(battle.getPlayers()[0].getId())?.id)
-      if (winner != null) {
-        session.getWaitQueue().enqueue(winner);
-        console.log(`Add player ${winner} to waitQueue`)
-        if (session.getWaitQueue().size() > 1) {
-          console.log("waitQueue Size large enough")
-
-          const player1Indexed = session.getWaitQueue().dequeue();
-          const player2Indexed = session.getWaitQueue().dequeue();
-
-          if (player1Indexed != undefined && player2Indexed != undefined) {
-          
-            let battleId = crypto.randomUUID();
-
-            const battle = new Battle(
-              battleId,
-              player1Indexed,
-              player2Indexed,
-              session.getHost()
-            );
-            battles.set(battleId, battle);
-            session.getBattles().enqueuefront(battle);
-
-            const gameCodeN = session.getGameCode();
-
-            console.log(`Connecting to ${socket.id}`)
-            for (const player of battle.getPlayers()) {
-              io.sockets.sockets.get(player.getId())?.join(battle.getId());
-            }
-            console.log(`Check to start battle ${battle.getId()}`)
-            io.to(battle.getId()).emit("battle_started", battle.getId());
-            proceedBattleTurn(io, socket, session, battle);
-          }
-
-        }
-      }
-      this.playerFinished += 1
-      console.log(this.playerFinished,session.getPlayers().getItems().length)
-      if ( this.playerFinished == session.getPlayers().getItems().length){
-        this.onBattlesEnded(session, io, socket)
-      }
-    })
-
-    io.sockets.sockets.get(battle.getPlayers()[1].getId())?.once("ready_next_battle", () => {
-      console.log("Server test 2 (Royale)", io.sockets.sockets.get(battle.getPlayers()[0].getId())?.id)
-      if (winner != null) {
-        session.getWaitQueue().enqueue(winner);
-        console.log(`Add player ${winner} to waitQueue`)
-        if (session.getWaitQueue().size() > 1) {
-          console.log("waitQueue Size large enough")
-
-          const player1Indexed = session.getWaitQueue().dequeue();
-          const player2Indexed = session.getWaitQueue().dequeue();
-
-          if (player1Indexed != undefined && player2Indexed != undefined) {
-          
-            let battleId = crypto.randomUUID();
-
-            const battle = new Battle(
-              battleId,
-              player1Indexed,
-              player2Indexed,
-              session.getHost()
-            );
-            battles.set(battleId, battle);
-            session.getBattles().enqueuefront(battle);
-
-            const gameCodeN = session.getGameCode();
-
-            console.log(`Connecting to ${socket.id}`)
-            for (const player of battle.getPlayers()) {
-              io.sockets.sockets.get(player.getId())?.join(battle.getId());
-            }
-            console.log(`Check to start battle ${battle.getId()}`)
-            io.to(battle.getId()).emit("battle_started", battle.getId());
-            proceedBattleTurn(io, socket, session, battle);
-
-          }
-        }
-      }
-      this.playerFinished += 1
-      console.log(this.playerFinished,session.getPlayers().getItems().length)
-      if (this.playerFinished == session.getPlayers().getItems().length){
-      this.onBattlesEnded(session, io, socket)
-      }
-    })
-  }
-
-	public onBattlesEnded(session: GameSession): void {
-    if (this.isSessionConcluded(session)) {
-      let winner = this.remainingPlayers[0];
-      let runnerUp = this.eliminatedPlayers[this.eliminatedPlayers.length-1];
-      const top2 = [  // Top 2 are the winner and runner up
-        winner.getPlayerState(),
-        runnerUp.getPlayerState()
-      ];
-      const gameCode = session.getGameCode();
-      this.io?.to(`game-${gameCode}`).emit("final-results-response", { playersToDisplay: top2 });
-      session.setFinalResults(top2);
-
-      console.log(`[FINAL RESULTS]: Winner: ${winner.getName()}, Runner up: ${runnerUp.getName()}`);
-		}
   }
 
 	public isSessionConcluded(session: GameSession): boolean {
