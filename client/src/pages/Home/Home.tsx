a1317b1a2f05221e58dcda9d2395dd320c8410ec
WillMGRichter <wric0006@student.monash.edu>
2025-09-02 15:31:39 +1000

    UI/2001.2 - Moved account / login to top right

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -57,10 +57,8 @@ export const Home = () => {
 
   return (
     <BlankPage>
-      <div className="flex flex-row h-1/2 w-full sm:items-end lg:items-center justify-around">
-        <LogoResizable className="lg:w-1/4 sm:h-3/4 lg:h-full" />
-      </div>
-      <div className="flex flex-col items-center justify-center w-1/2 h-1/2 lg:space-y-5 sm:space-y-30">
+      {/* Top-right login/account button */}
+      <div className="absolute top-10 right-10 z-50">
         {!loggedInUser ? (
           <ButtonGeneric
             color="ronchi"
@@ -78,7 +76,13 @@ export const Home = () => {
             <OutlineText size="large">ACCOUNT</OutlineText>
           </ButtonGeneric>
         )}
+      </div>
 
+      <div className="flex flex-row h-1/2 w-full sm:items-end lg:items-center justify-around">
+        <LogoResizable className="lg:w-1/4 sm:h-3/4 lg:h-full" />
+      </div>
+
+      <div className="flex flex-col items-center justify-center w-1/2 h-1/2 lg:space-y-5 sm:space-y-30">
         <ButtonGeneric
           color="ronchi"
           size="large"
@@ -91,6 +95,7 @@ export const Home = () => {
         <ButtonGeneric color="ronchi" size="large" onClick={renderJoinLobby}>
           <OutlineText size="large">JOIN GAME</OutlineText>
         </ButtonGeneric>
+
         <ButtonGeneric color="ronchi" size="large" onClick={renderAdventure}>
           <OutlineText size="large">ADVENTURE</OutlineText>
         </ButtonGeneric>
