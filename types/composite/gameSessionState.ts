d66fabdabbd731be2ae1c4fe3e4ed1ea3b8f984c
Luna Nagle <lunaout2020@gmail.com>
2025-09-02 19:42:59 +1000

    1001.8: Fix scoreboard not displaying on scored tournament.

--- types/composite/gameSessionState.ts
+++ types/composite/gameSessionState.ts
@@ -1,10 +1,12 @@
 import { BattlePhase, BattleState } from "./battleState";
 import { GameSessionData } from "../other/gameSessionData";
 import { Player } from "server/src/model/game/player"
+import { GameModeIdentifier } from "../single/gameMode";
 
 export interface GameSessionState {
   id: string;
   round: number;
+  mode: GameModeIdentifier;
   battleStates: BattleState[];
   gameSessionData: GameSessionData;
   currentPhase: BattlePhase;
