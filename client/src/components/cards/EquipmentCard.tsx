f913067f84ffccd5069bc97fe7be13535cb9bf43
Tinesia <tyuu0023@student.monash.edu>
2025-09-24 17:59:06 +1000

    3007: equipment card UI fix in bag popup - Fixed the width size of equipment card to match consumables in bag popup

--- client/src/components/cards/EquipmentCard.tsx
+++ client/src/components/cards/EquipmentCard.tsx
@@ -17,7 +17,7 @@ export const EquipmentCard = ({ equipment, onClick }: EquipmentProps) => {
     border border-[4px] border-blackCurrant
     rounded-2xl
     w-[40rem]
-    xl:w-[30rem]
+    xl:w-[40rem]
     p-[1rem]
     justify-center
     items-center
