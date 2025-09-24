fd8c6bba0f8cff4d45b2c7db7bd69defd34f8faf
Anika Kamleshwaran <anikakamleshwaran@gmail.com>
2025-09-25 01:45:45 +1000

    3007: fix start status countdown

--- server/src/model/game/status/startStatus.ts
+++ server/src/model/game/status/startStatus.ts
@@ -1,7 +1,16 @@
 import { Action } from "../action/action";
 import { Player } from "../player";
 import { Status } from "./status";
+import { StatusType } from "/types/single/statusType";
 
 export abstract class StartStatus extends Status {
+  constructor(
+    name: string,
+    description: string,
+    countDown: number = 0,
+    type: StatusType
+  ) {
+    super(name, description, countDown + 1, type);
+  }
   public abstract startingEffect(player: Player): void;
 }
