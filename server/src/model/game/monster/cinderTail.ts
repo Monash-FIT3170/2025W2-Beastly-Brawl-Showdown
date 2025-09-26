efd47027306efc897bf131aa1363df14c2138257
Derek Cao <derek.jcao@gmail.com>
2025-09-26 12:59:47 +1000

    3007: Level 5 updates - Added last stage colosseum base - Updated stat info to include max health - Added name inputs to all monster constructors - Added all monsters to enemy factory

--- server/src/model/game/monster/cinderTail.ts
+++ server/src/model/game/monster/cinderTail.ts
@@ -4,10 +4,10 @@ import { Balanced } from "../archetype/balanced";
 import { FlameLashAbilityAction } from "../action/ability/flameLash";
 
 export class CinderTail extends Monster {
-  constructor() {
+  constructor(name: string = "Cinder Tail") {
     super(
       MonsterIdentifier.CINDER_TAIL,
-      "Cinder Tail",
+      name,
       "A blazing dragon whose tail burns brighter than the sun. Its fiery lash sears through even the quickest dodges, leaving no escape from the flames.",
       new Balanced(),
       new FlameLashAbilityAction(),
