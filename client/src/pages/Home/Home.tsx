b5e64720d719329e4fe9e77f1b8dc27aa1f3f3c9
OmarSalem248 <osal0004@student.monash.edu>
2025-10-06 01:43:50 +1100

    1001.10: tried to get background music to work:                   reworked elements of homepage t get music, still not working

--- client/src/pages/Home/Home.tsx
+++ client/src/pages/Home/Home.tsx
@@ -1,66 +1,59 @@
-import React, { useState, useEffect ,useRef} from "react";
+import React, { useEffect, useState } from "react";
 import { FlowRouter } from "meteor/ostrio:flow-router-extra";
 import socket from "../../socket";
 import { ButtonGeneric } from "../../components/buttons/ButtonGeneric";
 import { OutlineText } from "../../components/texts/OutlineText";
 import LogoResizable from "../../components/logos/LogoResizable";
 import { BlankPage } from "../../components/pagelayouts/BlankPage";
-import { ButtonResizableText } from "../../components/buttons/ButtonResizableText";
 import { LoginPopup } from "./Login";
 import { IconButton } from "../../components/buttons/IconButton";
-import { BlackText } from "../../components/texts/BlackText";
+import {
+  initHomeMusic,
+  unmuteHomeMusic,
+  stopHomeMusic,
+  toggleMute,
+  isMuted,
+} from "../../audioController";
 
 export const Home = () => {
-  // Called on 'Host Lobby' button press
-  const renderConfigPage = () => {
-    FlowRouter.go("/host/choose-mode");
-  };
   const [showLogin, setShowLogin] = useState(false);
   const [loggedInUser, setLoggedInUser] = useState(false);
-  const audioRef = useRef<HTMLAudioElement | null>(null);
+  const [muted, setMuted] = useState(isMuted());
 
-  <audio ref={audioRef} src="public\music\Beastly_brawl_menu_screen_music.mp3" loop />
+  // initialize music on mount
   useEffect(() => {
+    initHomeMusic();
+
     socket.emit("check-login");
-
-    const handleLoginStatus = ({ loggedIn }) => {
-      setLoggedInUser(loggedIn);
-    };
-
+    const handleLoginStatus = ({ loggedIn }) => setLoggedInUser(loggedIn);
     socket.on("login-status", handleLoginStatus);
 
     return () => {
       socket.off("login-status", handleLoginStatus);
+      stopHomeMusic(); // stop if leaving to gameplay
     };
   }, []);
 
-  const createGame = () => {
-    socket.emit("create-game", {});
-    console.log("Game session created");
+  // unmute when any button is clicked
+  const handleUserAction = (callback: () => void) => {
+    unmuteHomeMusic();
+    callback();
   };
 
-  socket.on("new-game", ({ code }) => {
-    const codeString = code.toString();
-    FlowRouter.go(`/host/${codeString}`);
-  });
+  const renderConfigPage = () => handleUserAction(() => FlowRouter.go("/host/choose-mode"));
+  const renderJoinLobby = () => handleUserAction(() => FlowRouter.go("/join"));
+  const renderAdventure = () => handleUserAction(() => FlowRouter.go("/adventure/level-select"));
 
-  const renderJoinLobby = () => {
-    FlowRouter.go("/join");
-  };
-
-  const handleLoginSuccess = (username: string) => {
+  const handleLoginSuccess = () => {
     setShowLogin(false);
     setLoggedInUser(true);
   };
 
-  const handleExitLogin = () => {
-    setShowLogin(false);
-    console.log("Exit login");
-  };
+  const handleExitLogin = () => setShowLogin(false);
 
-  // Called on 'Adventure' button press
-  const renderAdventure = () => {
-    FlowRouter.go("/adventure/level-select");
+  const handleMuteToggle = () => {
+    toggleMute();
+    setMuted(isMuted());
   };
 
   return (
@@ -68,13 +61,13 @@ export const Home = () => {
       <div className="absolute lg:top-[3rem] lg:right-[3rem] top-[5rem] right-[5rem] items-center justify-center">
         {!loggedInUser ? (
           <ButtonGeneric
-            color={"ronchi"}
-            size={"squaremedium"}
+            color="ronchi"
+            size="squaremedium"
             onClick={() => setShowLogin(true)}
           >
             <div className="flex flex-col ">
-              <OutlineText size={"tiny"}>LOG</OutlineText>
-              <OutlineText size={"tiny"}>IN</OutlineText>
+              <OutlineText size="tiny">LOG</OutlineText>
+              <OutlineText size="tiny">IN</OutlineText>
             </div>
           </ButtonGeneric>
         ) : (
@@ -87,22 +80,27 @@ export const Home = () => {
           />
         )}
       </div>
+
+      {/* Optional mute/unmute toggle */}
+      <div className="absolute top-[3rem] left-[3rem]">
+        <ButtonGeneric color="ronchi" size="small" onClick={handleMuteToggle}>
+          <OutlineText size="tiny">{muted ? "UNMUTE" : "MUTE"}</OutlineText>
+        </ButtonGeneric>
+      </div>
+
       <div className="flex flex-row w-full sm:items-end lg:items-center justify-around">
         <LogoResizable className="lg:w-1/4 sm:h-3/4 lg:h-full" />
       </div>
+
       <div className="flex flex-col items-center justify-center w-1/2 h-1/2 lg:space-y-5 sm:space-y-30">
-        <ButtonGeneric
-          color="ronchi"
-          size="large"
-          onClick={renderConfigPage}
-          mobileHidden={"true"}
-        >
+        <ButtonGeneric color="ronchi" size="large" onClick={renderConfigPage} mobileHidden="true">
           <OutlineText size="large">HOST GAME</OutlineText>
         </ButtonGeneric>
 
         <ButtonGeneric color="ronchi" size="large" onClick={renderJoinLobby}>
           <OutlineText size="large">JOIN GAME</OutlineText>
         </ButtonGeneric>
+
         <ButtonGeneric color="ronchi" size="large" onClick={renderAdventure}>
           <OutlineText size="large">ADVENTURE</OutlineText>
         </ButtonGeneric>
