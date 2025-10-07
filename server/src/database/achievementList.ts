5bf3ac02ac7a968641671573902a4c9b5f18642c
WillMGRichter <wric0006@student.monash.edu>
2025-10-07 15:15:49 +1100

    2002- Created example achievement

--- server/src/database/achievementList.ts
+++ server/src/database/achievementList.ts
@@ -1,12 +1,14 @@
+import { create } from "node:domain";
 import { AchievementSchema, AchievementStatus } from  "./dbManager"
+import { NullAction } from "../model/game/action/null";
 
 
 export function createAchievementSchema(
   name: string,
   description: string,
   objectives?: Record<string, boolean | number>,
-  progress?: 0,
-  goal?: number,
+  progress: number = 0,
+  goal: number = 0,
   status: AchievementStatus = 'locked',
   hidden: boolean = false
 ): AchievementSchema {
@@ -22,12 +24,14 @@ export function createAchievementSchema(
 }
 // Create acheivements Here
 
-
+// undefined is too skip the optional parameters 
+const ach1 = createAchievementSchema("Can't stop winning", "Get 100 wins in any gamemode", undefined,undefined,100);
 
 
 
 
 //Add all created Achievements into this
 export const Achievements: AchievementSchema[] = [
+    ach1,
   
 ];
