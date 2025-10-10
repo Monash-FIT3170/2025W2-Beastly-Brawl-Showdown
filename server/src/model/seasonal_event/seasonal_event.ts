213b12c42a53af99aeb5bf6246ebe0b4cdcb3456
Naveen Sellathurai <Navboy44@outlook.com>
2025-10-10 12:58:05 +1100

    Feature/1012 - Added base code for SeasonalEvent Abstract class
    - Added a new directory 'seasonal_event' in server/src/model
    - Base code of the abstract class to have to set up the foundation for seasonal events (This will be extended later on)

---
+++ server/src/model/seasonal_event/seasonal_event.ts
@@ -0,0 +1,21 @@
+import { Monster } from "./../game/monster/monster";
+
+export abstract class SeasonalEvent {
+    private id: string;
+    private name: string;
+    private monster: Monster;
+    private description: string;
+
+    constructor (
+        id: string,
+        name: string,
+        monster: Monster,
+        description: string
+    ) {
+        this.id = id;
+        this.name = name;
+        this.monster = monster;
+        this.description = description;
+    }
+
+}
