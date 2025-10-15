8a2754ccbfc4ab1bbf65299764a338b684f0d5d4
Anika Kamleshwaran <akam0020@student.monash.edu>
2025-10-15 17:38:12 +1100

    3007: fixed typo + updated hint name for dragon scale

--- server/src/model/game/equipment/everbloomingFragment.ts
+++ server/src/model/game/equipment/everbloomingFragment.ts
@@ -1,26 +1,29 @@
 import { Player } from "../player";
 import { Equipment } from "./equipment";
-import { Resurection } from "../status/resurection";
+import { Resurrection } from "../status/resurrection";
 
 export class EverbloomingFragment extends Equipment {
   constructor() {
     super(
       "Everblooming Fragment",
-      "A crystal shard formed from the heart of an eternal lotus. Warm to the touch with a chance of resurection."
+      "A crystal shard formed from the heart of an eternal lotus. Warm to the touch with a chance of resurrection."
     );
   }
 
   public equip(player: Player): void {
     player.getMonster()?.incCritRate(this.strength);
-    player.addStatus(new Resurection())
+    player.addStatus(new Resurrection());
   }
   public unequip(player: Player): void {
     player.getMonster()?.incCritRate(this.strength);
-    
   }
 
   public getStatDescription(): string {
-    return "+" + this.strength + " Armour Class and 30% chance to resurrect with 50% HP";
+    return (
+      "+" +
+      this.strength +
+      " Armour Class and 30% chance to resurrect with 50% HP"
+    );
   }
 
   public calculateStrength(stage: number): void {
--- server/src/model/game/storyItem/dragonScale.ts
+++ server/src/model/game/storyItem/dragonScale.ts
@@ -5,7 +5,7 @@ export class DragonScale extends StoryItem {
     super(
       "Dragon Scale",
       "This deep red scale seems to have fallen off a wounded Cinder Tail. There's rumours these scales awaken many specialties of the Basalt.",
-      "THINK OF HINT",
+      "A dormant fire blazes within.",
       "DRAGON_SCALE"
     );
   }
