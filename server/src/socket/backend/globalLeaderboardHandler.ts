627453a0be6ab7e962460c6cb673a3da72b91c7f
Meng Siau <mengsiau@gmail.com>
2025-10-15 16:37:57 +1100

    feature/2003-New db fetch method for different attributes (num games played)

--- server/src/socket/backend/globalLeaderboardHandler.ts
+++ server/src/socket/backend/globalLeaderboardHandler.ts
@@ -1,13 +1,19 @@
 // responsible for handling global leaderboard requests
 
 import { Server, Socket } from "socket.io";
-import { getTopPlayersByWins } from "../../database/dbManager";
+import { getTopPlayersByWins, getTopPlayersByNumGamesPlayed } from "../../database/dbManager";
 
 export const globalLeaderboardHandler = (io: Server, socket: Socket) => {
   socket.on("fetchGlobalLeaderboard", async () => {
     try {
+      // Leaderboard will get top 5 for now ... Can increase later
+      // Fetch from database the top players 
       const topPlayersByWins = await getTopPlayersByWins(5);
+      const topPlayersByNumGamesPlayed = await getTopPlayersByNumGamesPlayed(5);
+
+      // Emit to client (GlobalLeaderboard)
       socket.emit("globalLeaderboardData", { success: true, data: [topPlayersByWins] });
+      
     } catch (error) {
       console.error("Error fetching global leaderboard:", error);
       socket.emit("globalLeaderboardData", {
