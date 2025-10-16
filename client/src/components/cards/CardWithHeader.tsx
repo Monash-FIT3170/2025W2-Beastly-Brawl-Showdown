9032e723103a0fff72dc252b3e7695f547b00381
Danniel You <dyou0009@student.monash.edu>
2025-10-16 21:33:12 +1100

    1013: Made the white more opaque for the card with header component - Also fixed a small layering issue with the header

--- client/src/components/cards/CardWithHeader.tsx
+++ client/src/components/cards/CardWithHeader.tsx
@@ -4,19 +4,19 @@ import { OutlineText } from "../../components/texts/OutlineText";
 interface CardWithHeaderProps {
   headerText: string;
   headerColor: "golden";
-  cardColor: "white";
+  cardColor: "opaqueWhite";
   children?: ReactNode;
 }
 
 export const CardWithHeader = ({ headerText, headerColor, cardColor, children }: CardWithHeaderProps) => {
   const colorToDisplay = {
     golden: "bg-goldenRod",
-    white:  "bg-plainWhite",
+    opaqueWhite: "bg-white/60",
   };
 
   return (
     <>
-      <div className={`${colorToDisplay[headerColor]} border-[0.2rem] border-blackCurrant text-center rounded-t-2xl px-[2rem] py-[1rem] shadow-md w-[90%] lg:w-[60%] z-10`}>
+      <div className={`${colorToDisplay[headerColor]} border-[0.2rem] border-blackCurrant text-center rounded-t-2xl px-[2rem] py-[1rem] shadow-md w-[90%] lg:w-[60%]`}>
         <OutlineText size="extraLarge">{headerText}</OutlineText>
       </div>
       <div className={`${colorToDisplay[cardColor]} border-[0.2rem] border-blackCurrant rounded-2xl shadow-lg px-[2rem] lg:px-[3rem] py-[2rem] w-[75%] lg:w-[75%] flex flex-col items-center justify-center -mt-[0.2rem]`}>
