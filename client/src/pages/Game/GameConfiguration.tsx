c513524b11a26fc68b6738a8593d140f60512d52
Danniel You <dyou0009@student.monash.edu>
2025-10-16 22:58:17 +1100

    1013: Redesigned the game configuration/settings page to be based off the new design in Figma - Now have the ability to change backgrounds - Chosen backgrounds are currently not set to the game yet

--- client/src/pages/Game/GameConfiguration.tsx
+++ client/src/pages/Game/GameConfiguration.tsx
@@ -3,29 +3,32 @@ import { FlowRouter } from "meteor/ostrio:flow-router-extra";
 import socket from "../../socket";
 import { ButtonGeneric } from "../../components/buttons/ButtonGeneric";
 import { OutlineText } from "../../components/texts/OutlineText";
-import { BlankPage } from "../../components/pagelayouts/BlankPage";
 import { GameModeIdentifier } from "../../../../types/single/gameMode";
 import { IconButton } from "../../components/buttons/IconButton";
-import { BaseCard } from "../../components/cards/BaseCard";
 import { Slider } from "../../components/sliders/Slider";
-import CardSelector from "../../components/selectors/CardSelector";
-import LogoResizable from "../../components/logos/LogoResizable";
+import { CardWithHeader } from "../../components/cards/CardWithHeader";
+import GameModeSelector from "../../components/selectors/GameModeSelector";
+import BackgroundThemeSelector from "../../components/selectors/BackgroundThemeSelector";
+import { OutlineTextResizable } from "../../components/texts/ResizableOutlineText";
 
 export const GameConfiguration = () => {
-  // Use selectedIndex to retrieve currently selected mode.
-  const [selectedIndex, setSelectedIndex] = useState(0);
+  // Use selectedGameModeIndex to retrieve currently selected mode.
+  const [selectedGameModeIndex, setSelectedGameModeIndex] = useState(0);
 
-  // Use selectedValue to retrieve currently selected number of rounds.
-  const [selectedValue, setSelectedValue] = useState(3);
+  // Use selectedBackgroundThemeIndex to retrieve currently background theme.
+  const [selectedBackgroundThemeIndex, setSelectedBackgroundThemeIndex] = useState(0);
+
+  // Use selectedSliderValue to retrieve currently selected number of rounds.
+  const [selectedSliderValue, setSelectedSliderValue] = useState(3);
 
   // Called on 'Host Lobby' button press
   const createGame = (mode: GameModeIdentifier) => {
-    socket.emit("create-game", { mode, selectedValue });
+    socket.emit("create-game", { mode, selectedSliderValue });
     console.log("Game session created");
   };
 
   // Define the title and description of each game mode.
-  const options = [
+  const gameModeOptions = [
     { 
       title: "Battle Royale", 
       description: "Fight until only one player remains standing!", 
@@ -35,10 +38,29 @@ export const GameConfiguration = () => {
       title: "Scored Tournament", 
       description: "Earn more points than your opponents over multiple rounds!", 
       mode: GameModeIdentifier.SCORING
-    },
+    }
   ];
 
-  const sliderDisabled = options[selectedIndex].title === "Battle Royale";
+  // Define the name of each background theme
+  const backgroundThemeOptions = [
+    {
+      name: "Forest",
+    },
+    {
+      name: "Basalt",
+    },
+    {
+      name: "Arctic",
+    },
+    {
+      name: "Marsh",
+    },
+    {
+      name: "Desert",
+    }
+  ];
+
+  const sliderDisabled = gameModeOptions[selectedGameModeIndex].title === "Battle Royale";
 
   // Received when Game Session is created. Takes user to 'Host Lobby' Page
   socket.on("new-game", ({ code }) => {
@@ -47,46 +69,51 @@ export const GameConfiguration = () => {
   });
 
   return (
-    (
-      <BlankPage>
-        <div className="flex lg:flex-row lg:h-1/4 sm:flex-col w-full">
-          <div className="flex flex-row w-1/8 sm:h-1/2">
-            <div className="lg:ml-2 lg:mt-2 sm:ml-6 sm:mt-6">
-              <IconButton
-                style="arrowleft"
-                iconColour="black"
-                buttonColour="red"
-                size="medium"
-                onClick={() => FlowRouter.go("/")}
-              />
-            </div>
-          </div>
-          <div className="flex flex-row lg:h-full lg:w-3/4 sm:h-3/4 lg:items-start sm:items-end justify-around">
-            <div className="lg:ml-2 lg:mt-2 sm:ml-6 sm:mt-6">
-              <BaseCard color="peach" width={50} height={8}>
-                <OutlineText size="extraLarge">GAME SETTINGS</OutlineText>
-              </BaseCard>
-            </div>
-          </div>
+    <div className="relative w-screen min-h-screen">
+      <div
+        className="fixed inset-0 -z-10"
+        style={{
+          backgroundImage: `url('https://spaces-bbs.syd1.cdn.digitaloceanspaces.com/assets/background/${backgroundThemeOptions[selectedBackgroundThemeIndex].name.toUpperCase()}.jpg')`,
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat"
+        }}
+      />
+      <div className="flex flex-col items-center justify-center min-h-screen w-full px-4 py-6 sm:px-8 sm:py-10 lg:px-16 lg:py-14">
+        <div className="absolute top-4 left-4 z-50">
+          <IconButton
+            style="arrowleft"
+            iconColour="black"
+            buttonColour="red"
+            size="medium"
+            onClick={() => FlowRouter.go("/")}
+          />
         </div>
-
-        <div className="flex flex-col items-center justify-center w-3/4 h-3/4 lg:space-y-5 sm:space-y-30">
+        <CardWithHeader headerText="GAME SETTINGS" headerColor="golden" cardColor="opaqueWhite">
+          <div className="flex flex-col items-center justify-center w-3/4 h-3/4 lg:space-y-5 sm:space-y-30">
             <div className="flex flex-row items-center justify-center h-7/16">
-              <CardSelector options={options} selectedIndex={selectedIndex} setSelectedIndex={setSelectedIndex}/>
+              <GameModeSelector options={gameModeOptions} selectedIndex={selectedGameModeIndex} setSelectedIndex={setSelectedGameModeIndex}/>
             </div>
             <div className="flex flex-row items-center justify-center h-1/16">
               <OutlineText size="large">Round Count:</OutlineText>
             </div>
             <div className="flex flex-row items-center justify-center h-3/16">
-              <Slider max={5} min= {1} selectedValue={selectedValue} setSelectedValue={setSelectedValue} isDisabled = {sliderDisabled}/>
+              <Slider max={5} min= {1} selectedValue={selectedSliderValue} setSelectedValue={setSelectedSliderValue} isDisabled = {sliderDisabled}/>
+            </div>
+            <div className="flex flex-row items-center justify-center h-1/16">
+              <OutlineText size="large">Background Theme</OutlineText>
+            </div>
+            <div className="flex flex-row items-center justify-center h-7/16">
+              <BackgroundThemeSelector options={backgroundThemeOptions} selectedIndex={selectedBackgroundThemeIndex} setSelectedIndex={setSelectedBackgroundThemeIndex}/>
             </div>
             <div className="flex flex-row items-center justify-center h-5/16">
-              <ButtonGeneric color="ronchi" size="longlarge" onClick={() => createGame(options[selectedIndex].mode)}>
-                  <OutlineText size="large">CREATE GAME</OutlineText>
+              <ButtonGeneric color="ronchi" size="medium" onClick={() => createGame(gameModeOptions[selectedGameModeIndex].mode)}>
+                <OutlineTextResizable size="medium">CREATE GAME</OutlineTextResizable>
               </ButtonGeneric>
             </div>
-        </div>
-      </BlankPage>
-    )
+          </div>
+        </CardWithHeader>
+      </div>
+    </div>
   );
 };
