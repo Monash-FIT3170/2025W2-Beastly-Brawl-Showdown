e6cd56c33646d8b1b20c3566d3c10d3dc51389a3
Danniel You <dyou0009@student.monash.edu>
2025-10-16 21:01:27 +1100

    1013: Removed the page background div that was part of the `CardWithHeader` component

--- client/src/components/cards/CardWithHeader.tsx
+++ client/src/components/cards/CardWithHeader.tsx
@@ -15,14 +15,13 @@ export const CardWithHeader = ({ headerText, headerColor, cardColor, children }:
   };
 
   return (
-    <div className="h-screen w-screen bg-[#f6e3b4] flex flex-col items-center justify-center">
+    <>
       <div className={`${colorToDisplay[headerColor]} border-[0.2rem] border-blackCurrant text-center rounded-t-2xl px-[2rem] py-[1rem] shadow-md w-[90%] lg:w-[60%] z-10`}>
         <OutlineText size="extraLarge">{headerText}</OutlineText>
       </div>
-
       <div className={`${colorToDisplay[cardColor]} border-[0.2rem] border-blackCurrant rounded-2xl shadow-lg px-[2rem] lg:px-[3rem] py-[2rem] w-[75%] lg:w-[75%] flex flex-col items-center justify-center -mt-[0.2rem]`}>
         {children}
       </div>
-    </div>
+    </>
   );
 };
