d0bc301a064d70291ad4682663655a72aa4d387b
Luna Nagle <lunaout2020@gmail.com>
2025-10-17 12:13:49 +1100

    1011: Add wiki page for how to play.

--- client/src/pages/Wiki/WikiTutorialPage.tsx
+++ client/src/pages/Wiki/WikiTutorialPage.tsx
@@ -2,10 +2,13 @@ import React, { useState } from "react";
 import { WikiHeader } from "../../components/wiki/WikiHeader";
 import { BlackText } from "../../components/texts/BlackText";
 
+
 export const WikiTutorialPage = () => {
     const annotations = "1.) Health Bars: Shows each monster's current and maximum HP.\n2.) Player Display: Shows each player’s name and the monster they’re using.\n3.) Timer: Shows how much time is left for each player to select a move. If time runs out, your turn will be skipped.\n4.) Ability Buttons: Shows each ability that you are able to use. Unavailable moves will be greyed out.\n5.) Ability Charges: Shows how many times you can use this ability this game. Not all abilities will have limited uses!"
 
     const abilities = "The goal of the game is to defeat your opponent by reducing their health points (HP) to zero. You can do this by using your monster's set of abilities to hurt your opponent and protect yourself.\nUsing an attack on an enemy will make a dice roll. If the roll plus your Attack Bonus is equal to or higher than the enemy's Armor Class (AC), the attack will hit and deal 5 points of damage. If the roll is lower than the enemy's AC, the attack will miss and deal no damage.\nAlternatively, you can use a defend to increase your AC by 5 for a turn, making it harder for your opponent to hit you.\nIn addition to attack and defend, each monster has 2 unique abilities with more advanced effects. Check the monster's ability descriptions for more information."
+
+    const annotatedImage = "/assets/BeastlyBrawlAnnotated.png";
     return (
 
         <div className="flex flex-col w-full h-full space-y-[1rem] overflow-y-scroll justify-start items-center m-[2rem]">
@@ -25,7 +28,7 @@ export const WikiTutorialPage = () => {
                     {abilities}
                 </BlackText>
 
-                
+                <img src={annotatedImage}/>
 
                 <BlackText size="medium">
                     {annotations}
