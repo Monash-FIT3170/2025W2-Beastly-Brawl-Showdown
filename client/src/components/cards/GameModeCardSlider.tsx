f9532290301dee4671be4b429eb8c21eae469c6a
Danniel You <dyou0009@student.monash.edu>
2025-10-19 21:53:38 +1100

    1013: Updated the slider component to be more like in the Figma hi-fi (i.e., the number appears inside the slider's circle control) - Temporarily removed the fixed height of the game mode cards

--- client/src/components/cards/GameModeCardSlider.tsx
+++ client/src/components/cards/GameModeCardSlider.tsx
@@ -22,10 +22,10 @@ export const GameModeCardSlider = ({
   setSelectedSliderValue
 }: GameModeCardSliderProps) => {
   return (
-    <div className="outline-[0.25rem] bg-peach p-6 rounded-xl w-120 h-75 text-center">
+    <div className="outline-[0.25rem] bg-peach p-6 rounded-xl w-120 h-full text-center">
       <OutlineText size="large">{cardData.title}</OutlineText>
       <BlackText size="medium">{cardData.description}</BlackText>
-      <div className="flex flex-row items-center justify-center h-1/16 p-6">
+      <div className="flex flex-row items-center justify-center h-1/16 pt-3">
         <OutlineText size="medium">{sliderText}</OutlineText>
       </div>
       <div className="flex flex-row items-center justify-center h-3/16">
