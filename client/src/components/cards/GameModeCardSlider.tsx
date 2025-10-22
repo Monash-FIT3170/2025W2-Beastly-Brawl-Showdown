624450e1460cb23d9784564cedd348d1f5abe757
dyou0009 <dyou0009@student.monash.edu>
2025-10-22 20:22:22 +1100

    Feature/1013 game settings UI redesign (#96)
    * 1013: Created a new component that combines a header attached to a card, to be used for the new game settings UI
    
    * 1013: Removed the page background div that was part of the `CardWithHeader` component
    
    * 1013: Made the white more opaque for the card with header component
    - Also fixed a small layering issue with the header
    
    * 1013: Split up the original CardSelector component into one for game mode and another for background theme
    - The design of both new selectors will need to be improved, but right now they are still functional
    
    * 1013: Redesigned the game configuration/settings page to be based off the new design in Figma
    - Now have the ability to change backgrounds
    - Chosen backgrounds are currently not set to the game yet
    
    * 1013: Added functionality so that the background theme that is selected on the game settings page is applied for the rest of the game session (affected pages listed below)
    - Game configuration/settings
    - Waiting screen
    - Host lobby
    - Host battles / match summary
    - Battles
    - Final results (battle royale and scoring)
    
    * 1013: Also applied the selected background theme to the winner, loser and draw screens
    
    * 1013: Fixed so that external clients can receive the same chosen background as the host (using different devices). Also removed the stored backgrounds once the game ends.
    - Previously, custom backgrounds only worked on the same device.
    
    * 1013: Updated so that kicking a player from the lobby removes their stored background
    
    * 1013: Reworked the game selector component so that it can display two types of cards: a plain card or a card with a slider. This is then used on the game settings page, to make the slider appear inside the card for scoring tournament, while for battle royale it doesn't display.
    
    * 1013: Updated the cards for the game mode selector to be visually similar to each other. These are styled based on the Figma mockups.
    
    * 1013: Updated the left/right buttons on the game settings page to appear similar to the Figma hi-fis
    
    * 1013: Added an animation effect when changing background themes on the game settings screen
    
    * 1013: Updated the slider component to be more like in the Figma hi-fi (i.e., the number appears inside the slider's circle control)
    - Temporarily removed the fixed height of the game mode cards
    
    * 1013: Adjusted the default number of rounds for scoring mode back to 3
    
    * 1013: Fixed the formatting of the game mode cards and the card sizes
    
    * 1013: Removed the vertical padding for the game settings header
    
    * 1013: Fixed bug where scoring tournament didn't play until the selected number of rounds
    
    * 1013: Added "Colosseum" and "Endless" background themes
    
    * 1013: Changed the opaque background of various screens to be white instead of orangey so that it doesn't conflict with the different background themes
    
    * 1013: Fixed some code formatting
    
    * 1013: Fixed bug where the selected background was not shown anymore for the waiting screen
    - The code for this was overriden in the last merge

---
+++ client/src/components/cards/GameModeCardSlider.tsx
@@ -0,0 +1,36 @@
+import React, { useState } from "react";
+import { GameModeCardData } from "../../types/SelectorCardData";
+import { Slider } from "../sliders/Slider";
+import { BlackText } from "../texts/BlackText";
+import { OutlineText } from "../texts/OutlineText";
+
+interface GameModeCardSliderProps {
+  cardData: GameModeCardData;
+  sliderText: string;
+  sliderMin: number;
+  sliderMax: number;
+  selectedSliderValue: number;
+  setSelectedSliderValue: (index: number) => void;
+}
+
+export const GameModeCardSlider = ({
+  cardData,
+  sliderText,
+  sliderMin,
+  sliderMax,
+  selectedSliderValue,
+  setSelectedSliderValue
+}: GameModeCardSliderProps) => {
+  return (
+    <div className="outline-[0.25rem] bg-peach p-6 rounded-xl w-120 h-65 text-center">
+      <OutlineText size="large">{cardData.title}</OutlineText>
+      <BlackText size="medium">{cardData.description}</BlackText>
+      <div className="flex flex-row items-center justify-center h-1/16 pt-5">
+        <OutlineText size="medium">{sliderText}</OutlineText>
+      </div>
+      <div className="flex flex-row items-center justify-center h-3/16 pt-10">
+        <Slider max={sliderMax} min= {sliderMin} selectedValue={selectedSliderValue} setSelectedValue={setSelectedSliderValue} />
+      </div>
+    </div>
+  );
+};
--- client/src/components/sliders/Slider.tsx
+++ client/src/components/sliders/Slider.tsx
@@ -1,21 +1,28 @@
-import React, { useState } from "react";
+import React from "react";
 import { OutlineText } from "../texts/OutlineText";
 
-// Add `isDisabled` prop type
 interface SliderProps {
-  max?: number, 
-  min?: number,
+  max?: number;
+  min?: number;
   selectedValue: number;
   setSelectedValue: (index: number) => void;
   isDisabled?: boolean;
 }
 
-export const Slider: React.FC<SliderProps> = ({ max = 100, min = 0, selectedValue, setSelectedValue, isDisabled = false }) => {
+export const Slider: React.FC<SliderProps> = ({
+  max = 100,
+  min = 0,
+  selectedValue,
+  setSelectedValue,
+  isDisabled = false,
+}) => {
+  const percentage = ((selectedValue - min) / (max - min)) * 100;
 
   return (
     <div
-      className={`flex flex-col items-center space-y-4 w-full max-w-md mx-auto mt-10 
-      ${isDisabled ? "opacity-50 cursor-not-allowed" : ""}`}
+      className={`relative flex flex-col items-center space-y-4 w-full max-w-md mx-auto mt-3 ${
+        isDisabled ? "opacity-50 cursor-not-allowed" : ""
+      }`}
     >
       <input
         type="range"
@@ -24,12 +31,20 @@ export const Slider: React.FC<SliderProps> = ({ max = 100, min = 0, selectedValu
         value={selectedValue}
         onChange={(e) => setSelectedValue(Number(e.target.value))}
         disabled={isDisabled}
-        className={`w-full h-2 bg-gray-200 rounded-lg appearance-none border-[2px] border-blackCurrant 
-          ${isDisabled ? "cursor-not-allowed" : "cursor-pointer"}`}
+        className={`w-full h-2 bg-gray-300 rounded-lg border-2 border-blackCurrant ${
+          isDisabled ? "cursor-not-allowed" : "cursor-pointer"
+        }`}
       />
-      <span className="text-lg font-medium">
-        <OutlineText size="large">{selectedValue}</OutlineText>
-      </span>
+
+      <div
+        className="absolute w-10 h-10 bg-pictonBlue border-2 border-black rounded-full flex items-center justify-center pointer-events-none"
+        style={{
+          left: `calc(${percentage}% - 1.25rem)`,
+          top: 'calc(50% - 1.7rem)'
+        }}
+      >
+        <OutlineText size="small">{selectedValue}</OutlineText>
+      </div>
     </div>
   );
 };
